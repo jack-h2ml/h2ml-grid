3dc264224b2fbb0e01959adde87d21669a81ddfb
jacknotman <jack@h2ml.agency>
2024-04-24 17:19:58 +0100

    Webpack Revision Build

--- blocks/build/gridArea/index.asset.php
+++ blocks/build/gridArea/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n', 'wp-primitives'), 'version' => 'ed84c138c03c1b6f4fc2');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n', 'wp-primitives'), 'version' => '2a515c1996d114fb158a');
