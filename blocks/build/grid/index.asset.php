e71aec5d4b0b134e20eda906ffc5c39d5442e9bb
jacknotman <jack@h2ml.agency>
2024-04-24 16:41:29 +0100

    Webpack Revision Build

--- blocks/build/grid/index.asset.php
+++ blocks/build/grid/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-block-serialization-default-parser', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '9db5d01be4c4948fd531');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-block-serialization-default-parser', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '7bb4d25e94cf42b9128f');
