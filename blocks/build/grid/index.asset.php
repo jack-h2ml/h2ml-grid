b3190d5be7c9fec85db6662b0b0b17d751354b51
jacknotman <jack@h2ml.agency>
2024-05-13 09:47:49 +0100

    Webpack Revision Build

--- blocks/build/grid/index.asset.php
+++ blocks/build/grid/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-block-serialization-default-parser', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '5c4629084fa4429cba71');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-block-serialization-default-parser', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'f9b7b3556a5b3e6326ea');
--- h2ml-grid.php
+++ h2ml-grid.php
@@ -3,7 +3,7 @@
 /*
 Plugin Name: H2ML Grid
 Author: Jack Notman
-Version: 1.0.35
+Version: 1.0.36
 */
 
 /**
